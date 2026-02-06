9f6489617186eec667e9ea0eac192c806677198c
marcinchudaszek-cmd <marcin.chudaszek@gmail.com>
2026-02-06 14:20:04 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,58 +1,58 @@
 const CACHE_NAME = 'muzyka-v3';
 const urlsToCache = [
- './',
- './index.html',
- './style.css',
- './app.js',
- './manifest.json',
- './icon-192.png',
- './icon-512.png'
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-Instalacja
+// Instalacja
 self.addEventListener('install', function(event) {
- self.skipWaiting();
- event.waitAdy(
- caches.open(CACHE_NAME)
- .then(function(cache) {
- console.log('Cache otwarty');
- return cache.addAll(urlsToCache);
- })
- );
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Cache otwarty');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-Pobieranie zasobÓw - Network First
+// Pobieranie zasobów - Network First
 self.addEventListener('fetch', function(event) {
- event.respondWith(
- fetch(event.request)
- .then(function(response) {
- if (response && response.status === 200) {
- var responseClone = response.clone();
- caches.open(CACHE_NAME).then(function(cache) {
- cache.put(event.request, responseClone);
- });
- }
- odpowiedź;
- })
- .catch(function() {
- return caches.match(event.request);
- })
- );
+  event.respondWith(
+    fetch(event.request)
+      .then(function(response) {
+        if (response && response.status === 200) {
+          var responseClone = response.clone();
+          caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(event.request, responseClone);
+          });
+        }
+        return response;
+      })
+      .catch(function() {
+        return caches.match(event.request);
+      })
+  );
 });
 
-Aktualizacja - usuŤ" stare cache
+// Aktualizacja - usuń stare cache
 self.addEventListener('activate', function(event) {
- event.waitAdy(
- caches.keys().then(function(cacheNames) {
- return Promise.all(
- cacheNames.filter(function(cacheName) {
- return cacheName !== CACHE_NAME;
- }).map(function(cacheName) {
- return caches.delete(cacheName);
- })
- );
- }).then(function() {
- return self.clients.claim();
- })
- );
-}); 
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName !== CACHE_NAME;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
